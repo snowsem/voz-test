215e8de6e04a781516a266ff81caff1d0d0c709b
snowsem <snowsem@rambler.ru>
2017-06-14 16:29:09 +0300

    routing rules

--- controllers/CityController.php
+++ controllers/CityController.php
@@ -57,7 +57,7 @@ class CityController extends Controller {
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
 
-            Yii::$app->session->setFlash('contactFormSubmitted');
+            Yii::$app->session->setFlash('added city');
 
             return $this->redirect('/city');
 
@@ -74,6 +74,25 @@ class CityController extends Controller {
         $model = City::findOne($id);
         $model->delete();
         return $this->redirect(['index']);
+    }
+
+    public function actionEdit($id) {
+
+        $model = City::findOne($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->update()) {
+
+            Yii::$app->session->setFlash('editing city');
+
+            return $this->redirect('/city');
+
+        } else {
+
+            return $this->render('new', [
+                'model' => $model,
+            ]);
+        }
+
     }
 
 }
